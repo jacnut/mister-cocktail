bc40007173b9fb801d887ee7e1a9db2caaecddcf
Jacques De Liedekerke <jacnut@gmail.com>
2015-02-20 18:52:36 +0100

    css

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -7,7 +7,7 @@ class Cocktail < ActiveRecord::Base
   accepts_nested_attributes_for :doses
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300>", thumb: "100x100#" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
